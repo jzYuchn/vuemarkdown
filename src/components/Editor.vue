6c8947ed61513bd764a5802eb175db3ae622f0c3
kusaoisii <dkadd911@gmail.com>
2018-12-05 22:56:13 +0900

    fix

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="editor">
     <h1>エディター画面</h1>
-    <button @click="console">con</button>
+    <span>{{user.displayName}}</span>
     <button @click="logout">s</button>
   </div>
 </template>
@@ -9,12 +9,11 @@
 <script>
 export default{
   name:"editor",
-  props : ["user"],
+  props : ['user'],
   data(){
     return {};
   },
   methods : {
-    console: function(){console.log(user)},
     logout: function(){
       firebase.auth().signOut();
     }
