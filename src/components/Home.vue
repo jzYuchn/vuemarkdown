868478cad8c06563a59876471b7bc83bc22cb701
kusaoisii <dkadd911@gmail.com>
2018-12-05 21:21:25 +0900

    add func

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+f<template>
+  <div class="home">
+    <h1>{{msg}}</h1>
+    <button @click="googleLogin">Googleアカウントでログイン</button>
+    <button @click="console"> console </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name : "home" ,
+  data(){
+    return {
+      msg : "welcom to myapp"
+    };
+  },
+  methods : {
+    googleLogin : function(){
+      console.log("e");
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    },
+    console : function(){
+      console.log("conso");
+    }
+  }
+};
+</script>
