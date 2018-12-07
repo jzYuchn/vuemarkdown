1cf8dd0e87ddabff5c0b039f53e3240fc35bc463
kusaoisii <dkadd911@gmail.com>
2018-12-07 18:37:45 +0900

    to 35p

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,6 +3,10 @@
     <h1>エディター画面</h1>
     <span>{{user.displayName}}</span>
     <button @click="logout">ログアウト</button>
+    <div class="editoroWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+    </div>
+    <div class="preview" v-html="preview()"></div>
   </div>
 </template>
 
@@ -11,12 +15,30 @@ export default{
   name:"editor",
   props : ['user'],
   data(){
-    return {};
+    return {
+      markdown : ""
+    };
   },
   methods : {
     logout: function(){
       firebase.auth().signOut();
-    }
+    },
+    preview : function (){
+      return marked(this.markdown);
+    },
   }
 };
 </script>
+<style lang="scss" scoped>
+.editoroWrapper{
+  display : flex;
+}
+.markdown {
+  width: 50%;
+  height :500px;
+}
+.preview {
+  width: 50%;
+  text-align: left;
+}
+</style>
