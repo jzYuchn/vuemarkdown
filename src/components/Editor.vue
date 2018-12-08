a3fa72b9670a960f1ac4b72f95cccaa26a1bdf1c
kusaoisii <dkadd911@gmail.com>
2018-12-08 15:01:25 +0900

    add

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -10,6 +10,7 @@
             {{ displayTitle(memo.markdown)}}
           </p>
         </div>
+        <button class="addMemoBtn" @click="addMemo">メモの追加</button>
         <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
         <div class="preview" v-html="preview()"></div>
       </div>
